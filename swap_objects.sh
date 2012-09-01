4f04b6c302dabb105cb5c0550f79e4b61d842a32
Jonathan T. Beard <jt.beard@gmail.com>
2012-09-01 13:38:03 -0400

    Swap objects now used on region2

--- swap_objects.sh
+++ swap_objects.sh
@@ -1,4 +1,4 @@
 #cd ~/Desktop/df/yadfm
-rm -rf data/save/region1/raw/objects
-cp -R raw/objects data/save/region1/raw/objects
+rm -rf data/save/region2/raw/objects
+cp -R raw/objects data/save/region2/raw/objects
 #dc
